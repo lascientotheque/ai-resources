27669846a119df3917a5b4685502594085093894
Yannael_LB <>
2022-03-04 13:32:59 +0100

    Update

---
+++ category_meaning_fr.R
@@ -0,0 +1,43 @@
+all_languages<-c("Toutes les langues",
+               "FR",
+               "EN",
+               "Autre")
+
+all_resource_types <- c("Tous les types de ressources",
+  "Cours",
+                  "Fiches pédagogiques",
+                  "Outils interactifs",
+                  "Tutoriels")
+
+all_ages <- c("Tous les âges",
+              "<6",
+            "6-10",
+            "10-14",
+            "14-18",
+            ">18")
+
+all_categories <- c("Toutes les catégories",
+                    "Culture générale",
+                 "Algorithme",
+                 "Apprentissage",
+                 "Programmation",
+                 "IoT-Robotique",
+                 "Ethique"
+                 )
+
+all_data_types <- c("Tous les types de données",
+                    "Image",
+                  "Texte",
+                  "Son",
+                  "Réseaux sociaux",
+                  "Publicités",
+                  "Données personnelles")
+
+all_connectivity <- c("Débranché/Connecté",
+                      "Débranché",
+                  "Connecté")
+
+all_other_keywords <- c("Tous les mots-clefs",
+                        "Magie",
+                       "Jeux")
+
